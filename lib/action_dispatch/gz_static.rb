3a1408707f63e483d9ade00aab782816fe348ad2
John Joseph Bachir <j@jjb.cc>
2013-11-29 02:48:02 -0500

    reimplement as subclass of ActionDispatch::Static

--- lib/action_dispatch/gz_static.rb
+++ lib/action_dispatch/gz_static.rb
@@ -1,89 +1,40 @@
 # encoding: utf-8
-require 'rack/utils'
 require 'rack/mime'
 require 'rack/file'
-require 'active_support/core_ext/uri'
 
 require 'action_dispatch/gz_railtie' if defined?(Rails)
+require 'action_dispatch/middleware/static'
+require 'action_controller'
 
 module ActionDispatch
-  class GzStatic
-    def initialize(app, root, cache_control=nil)
-      @app           = app
-      @root          = root.chomp('/')
-      @compiled_root = /^#{Regexp.escape(@root)}/
+  class GzStatic < Static
+    def call(env)
+      return super unless %w[GET HEAD].include? env['REQUEST_METHOD']
+      path = env['PATH_INFO'].chomp('/')
+      return super unless @file_handler.match?(path)
 
-      # I wish there was a better way to do this. Rack::File changed its
-      # interface after 1.4.1 and I don't know of a way to sniff out the change
-      # without comparing the gem version
-      if Gem.loaded_specs['rack'].version > Gem::Version.new('1.4.1')
-        cache_control = {'Cache-Control' => cache_control}
+      compressed_path = "#{path}.gz"
+
+      compressed_exists = @file_handler.match?(compressed_path)
+      wants_compressed  = env['HTTP_ACCEPT_ENCODING'] =~ /\bgzip\b/
+
+      if wants_compressed && compressed_exists
+        env["PATH_INFO"] = compressed_path
       end
 
-      @file_server   = ::Rack::File.new(@root, cache_control)
-    end
+      status, headers, body = super
 
-    def match?(path)
-      path = path.dup
+      if compressed_exists
+        headers['Vary'] = 'Accept-Encoding'
 
-      full_path = path.empty? ? @root : File.join(@root, escape_glob_chars(unescape_path(path)))
-      paths = "#{full_path}#{ext}"
-
-      matches = Dir[paths]
-      matches.detect { |m| File.file?(m) }
-    end
-
-    def call(env)
-      case env['REQUEST_METHOD']
-      when 'GET', 'HEAD'
-        path = env['PATH_INFO'].chomp('/')
-        if match = match?(path)
-
-          compressed_match = "#{match}.gz"
-          compressed_exists = File.file?(compressed_match)
-            
-          wants_compressed = !!(env['HTTP_ACCEPT_ENCODING'] =~ /\bgzip\b/)
-
-          if wants_compressed && compressed_exists
-            mime = Rack::Mime.mime_type(::File.extname(match), 'text/plain')
-            match = compressed_match
-          end
-
-          match.sub!(@compiled_root, '')
-          env["PATH_INFO"] = ::Rack::Utils.escape(match)
-          status, headers, body = @file_server.call(env)
-
-          if compressed_exists
-            headers['Vary'] = 'Accept-Encoding'
-
-            if wants_compressed
-              headers['Content-Encoding'] = 'gzip'
-              headers['Content-Type'] = mime if mime
-            end
-          end
-
-          return [status, headers, body]
+        if wants_compressed
+          headers['Content-Encoding'] = 'gzip'
+          mime = Rack::Mime.mime_type(::File.extname(path), 'text/plain')
+          headers['Content-Type'] = mime if mime
         end
       end
 
-      @app.call(env)
-    end
-
-    def ext
-      @ext ||= begin
-        #ext = ::ActionController::Base.default_static_extension
-        ext = ".html"
-        "{,#{ext},/index#{ext}}"
-      end
-    end
-
-    def unescape_path(path)
-      URI.parser.unescape(path)
-    end
-
-    def escape_glob_chars(path)
-      path.force_encoding('binary') if path.respond_to? :force_encoding
-      path.gsub(/[*?{}\[\]]/, "\\\\\\&")
+      [status, headers, body]
     end
   end
 end
